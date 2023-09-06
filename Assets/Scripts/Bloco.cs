65dbd93e8bcf23f6ab7a71014ec7de096e7ef0b9
Higor V.L <higor-lopo@hotmail.com>
2023-09-06 18:27:53 -0400

    Adicionando som no jogo

--- Assets/Scripts/Bloco.cs
+++ Assets/Scripts/Bloco.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Bloco : MonoBehaviour {
     private Rigidbody2D rb2d;
     private BoxCollider2D colisor;
-    private bool colisaoComBola = false; // Variável para rastrear se houve colisão com a bola
 
     // Start is called before the first frame update
     void Start() {
@@ -15,11 +14,12 @@ public class Bloco : MonoBehaviour {
 
     // Chamado quando ocorre uma colisão
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("bola") || collision.gameObject.CompareTag("plataforma")) {
+        //if (collision.gameObject.CompareTag("bola") || collision.gameObject.CompareTag("plataforma")) {
             Physics2D.IgnoreCollision(colisor, collision.collider, true);
+            colisor.isTrigger = true;
             rb2d.constraints = RigidbodyConstraints2D.None;
             
-            // Desativar colisão com objetos que têm as Tags "bola" e "plataforma"
-        }
+           
+        //}
     }
 }
--- Assets/Scripts/Destruidor.cs
+++ Assets/Scripts/Destruidor.cs
@@ -3,23 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Destruidor : MonoBehaviour {
-    public AudioSource audioSource; // Referência para o AudioSource no Inspector
+
+    public AudioSource audioSource;
+    
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("bola")) {
-            GameObject colisor = collision.collider.gameObject;
-            GameObject.Destroy(colisor);
-            Application.Quit();
-        } else {
-            StartCoroutine(DestruirColisorDepoisDeUmSegundo(collision.collider.gameObject));
-        }
+        GameObject colisor = collision.collider.gameObject;
+        GameObject.Destroy(colisor); 
+        audioSource.Play();
     }
 
-    IEnumerator DestruirColisorDepoisDeUmSegundo(GameObject objetoParaDestruir) {
-        // Tocar o som no início da função
-        audioSource.Play();
-
-        yield return new WaitForSeconds(0.5f);
-        GameObject.Destroy(objetoParaDestruir); // Destruir o objeto colisor após 0.5 segundos
+    private void OnTriggernter2D(Collider2D colisor){
+        GameObject.Destroy(colisor.gameObject);
     }
 }
